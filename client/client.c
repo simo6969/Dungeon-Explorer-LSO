d618533a8d8f41f112a9d4a319575eb089bc398d
simo <simone.ascione04@gmail.com>
2026-04-16 18:13:03 +0200

    Server multi-thread, aggiornamento server.c e client.c

--- client/client.c
+++ client/client.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include "protocol.h"
 
 int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
     printf("[CLIENT-INFO] Benvenuto in Dungeon Explorer!\n");
-    printf("[CLIENT-INFO] Tentativo di connessione al server...\n");
+    printf("[CLIENT-INFO] Preparazione dell'equipaggiamento di rete...\n");
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("[CLIENT-ERR] Errore nella creazione del socket\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORTA_SERVER);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("[CLIENT-ERR] Indirizzo IP non valido o non supportato\n");
+        return -1;
+    }
+
+    printf("[CLIENT-INFO] Tentativo di connessione al Server (127.0.0.1:%d)...\n", PORTA_SERVER);
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("[CLIENT-ERR] Connessione fallita. Il Server è acceso?\n");
+        return -1;
+    }
+
+    printf("[CLIENT-INFO] Connessione stabilita con successo! Sei nel Dungeon.\n");
+
+    close(sock);
     
-    // Qui in seguito scriveremo il codice per connettersi al socket
-    
+    printf("[CLIENT-INFO] Disconnessione. A presto!\n");
+
     return 0;
 }
--- server/server.c
+++ server/server.c
@@ -1,20 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h> 
 #include "protocol.h"
 
-int main() {
-    // Questa riga magica disabilita il "cassetto" e forza il C 
-    // a stampare tutto immediatamente nel terminale Docker.
-    setbuf(stdout, NULL); 
 
-    printf("[SERVER-INFO] Server di Dungeon Explorer in fase di avvio...\n");
-    printf("[SERVER-INFO] In ascolto sulla porta %d...\n", PORTA_SERVER);
+void *gestisci_giocatore(void *arg) {
+    int client_socket = *(int *)arg;
     
-    // Il server resta acceso
-    while(1) {
-        sleep(10); 
+    free(arg); 
+
+    printf("[THREAD] Nuovo thread avviato! Mi sto occupando del giocatore sul socket %d\n", client_socket);
+
+    printf("[THREAD] Il giocatore %d sta esplorando...\n", client_socket);
+    sleep(15); 
+
+    printf("[THREAD] Il giocatore %d ha abbandonato il server. Chiudo la sua connessione.\n", client_socket);
+    close(client_socket);
+
+    pthread_exit(NULL); 
+}
+
+int main() {
+    setbuf(stdout, NULL);
+
+    int server_socket, client_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    socklen_t addrlen = sizeof(address);
+
+    printf("[SERVER-INFO] Avvio Server Multi-Thread...\n");
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("[SERVER-ERR] Creazione socket fallita");
+        exit(EXIT_FAILURE);
     }
-    
+
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("[SERVER-ERR] Setsockopt fallita");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORTA_SERVER);
+
+    if (bind(server_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("[SERVER-ERR] Bind fallito");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, MAX_PLAYERS) < 0) {
+        perror("[SERVER-ERR] Listen fallito");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[SERVER-INFO] Il Centralino è aperto sulla porta %d...\n", PORTA_SERVER);
+
+    while(1) {
+        printf("[SERVER-INFO] In attesa di nuovi giocatori...\n");
+
+        if ((client_socket = accept(server_socket, (struct sockaddr *)&address, &addrlen)) < 0) {
+            perror("[SERVER-ERR] Accept fallita");
+            continue; 
+        }
+
+        printf("[SERVER-INFO] Un giocatore ha bussato alla porta! Affido il giocatore a un Thread...\n");
+
+        int *new_sock = malloc(sizeof(int));
+        *new_sock = client_socket;
+
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, gestisci_giocatore, (void *)new_sock) < 0) {
+            perror("[SERVER-ERR] Impossibile creare il thread");
+            free(new_sock);
+            close(client_socket);
+        } else {
+            pthread_detach(thread_id);
+        }
+    }
+
+    close(server_socket);
     return 0;
 }
